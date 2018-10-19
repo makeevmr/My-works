5a9749ff0106a29444bf1422e3100eb001bec3cd
makeevmr <art.makeev2016@yandex.ru>
2018-10-19 20:19:54 +0300

    Домашнее задание 2

---
+++ Дз 2/Программа 2.py
@@ -0,0 +1,35 @@
+from unittest import TestCase, main
+
+def perfect_numb(a):
+    if type(a)!=str:
+        s=0
+        x=0
+        if a>1 and type(a)==int:
+            x=1
+            for i in range (1,a//2+1):
+                if a%i==0:
+                    s=s+i
+            if a==s :
+                return True
+            else:
+                return False
+    try:
+        p=1/x
+    except:
+        print('Введенное число не соотвтетствует условию совершенных чисел')
+    
+        
+class test_perfect_numb(TestCase):
+    def test_true(self):
+        self.assertTrue(perfect_numb(6))
+        self.assertTrue(perfect_numb(28))
+        self.assertTrue(perfect_numb(496))
+        self.assertTrue(perfect_numb(8128))
+    def test_false(self):
+        self.assertFalse(perfect_numb(2))
+        self.assertFalse(perfect_numb(9))
+        self.assertFalse(perfect_numb(7))
+        
+                
+            
+main()
